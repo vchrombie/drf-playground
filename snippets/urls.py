7f10bf0e7ded576ac674bea7237b31fcb452221a
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-01-25 19:44:25 +0530

    Update views and urls using class-based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,8 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list, name='snippet_list'),
-    path('snippets/<int:pk>/', views.snippet_detail, name='snippet_detail'),
+    path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
