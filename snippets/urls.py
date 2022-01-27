86d98fc37e252ced4f2eb5266f1531385d379e35
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-01-27 16:40:55 +0530

    Use Routers to create the routes for the ViewSets created previously

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,48 +1,14 @@
 from django.urls import path, include
 
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
-from snippets.views import SnippetViewSet, UserViewSet, api_root
+from snippets.views import SnippetViewSet, UserViewSet
 
-snippets_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
+router = DefaultRouter()
 
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy',
-})
-
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight',
-},
-    renderer_classes=[renderers.StaticHTMLRenderer]
-)
-
-users_list = UserViewSet.as_view({
-    'get': 'list',
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve',
-})
-
+router.register(r'snippets', viewset=SnippetViewSet)
+router.register(r'users', viewset=UserViewSet)
 
 urlpatterns = [
-    path('', api_root, name='api-root'),
-    path('snippets/', snippets_list, name='snippets-list'),
-    path('snippet/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippet/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', users_list, name='users-list'),
-    path('user/<int:pk>/', user_detail, name='user-detail'),
+    path('', include(router.urls)),
 ]
-
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -3,21 +3,12 @@ from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse(viewname='users-list', request=request, format=format),
-        'snippets': reverse(viewname='snippets-list', request=request, format=format),
-    })
 
 
 class SnippetViewSet(
