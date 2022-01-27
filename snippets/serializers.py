cb8d49b8b3460d42557350ea30ee59f60a91e211
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-01-27 15:38:58 +0530

    Add support for displaying code as highlighted
    This commit also changes the serializers to extend
    HyperlinkedModelSerializer, instead of ModelSerializer
    since it includes a url field by using HyperlinkedIdentityField.
    The relationships also use HyperlinkedRelatedField, instead
    of PrimaryKeyRelatedField.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,31 +5,39 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight',
+        format='html',
+    )
 
     class Meta:
         model = Snippet
         fields = (
+            'url',
             'id',
+            'highlight',
+            'owner',
             'title',
             'code',
             'linenos',
             'language',
             'style',
-            'owner',
         )
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedIdentityField(
         many=True,
-        queryset=Snippet.objects.all(),
+        view_name='snippet-detail',
+        read_only=True,
     )
 
     class Meta:
         model = User
         fields = (
+            'url',
             'id',
             'username',
             'snippets',
--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -69,3 +70,14 @@ class UserDetail(
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(
+    generics.GenericAPIView
+):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlight)
